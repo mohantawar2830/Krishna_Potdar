738416b2c73160f7eced0776689f718ea97cb783
mohantawar <tawarmr19.extc@coep.ac.in>
2021-11-19 19:10:25 +0530

    initial commit

---
+++ src/main/java/resource/Demo.java
@@ -0,0 +1,9 @@
+package resource;
+
+public class Demo {
+
+	public static void main(String[]args) {
+		System.out.println("hello github");
+	}
+	
+}
---
+++ src/test/java/New_Project/New_Project/AppTest.java
@@ -0,0 +1,20 @@
+package New_Project.New_Project;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
